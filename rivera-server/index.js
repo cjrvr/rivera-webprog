5547d79bcffc8fd6c7ca94fd8476a908b31e2e7e
cjrvr <cjrivera0809@gmail.com>
2026-05-19 16:13:04 +0800

    lab-act7

---
+++ rivera-server/index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API Running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
